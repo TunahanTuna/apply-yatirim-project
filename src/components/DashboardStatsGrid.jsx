05e08723d1d74457651444f83d27607963424f0a
Tuna <tuna@applyyatirim.com>
2023-09-01 11:39:24 +0300

    dasboard created

--- src/components/DashboardStatsGrid.jsx
+++ src/components/DashboardStatsGrid.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 export default function DashboardStatsGrid() {
     return (
-        <div className="flex gap-4">
-            <div className="bg-white"></div>
-            <div className="bg-white"></div>
-            <div className="bg-white"></div>
-            <div className="bg-white"></div>
+        <div className="flex gap-4 w-full">
+            <div className="bg-white rounded-sm flex-1 p-4 border border-gray-200 flex items-center">a</div>
+            <div className="bg-white rounded-sm flex-1 p-4 border border-gray-200 flex items-center">b</div>
+            <div className="bg-white rounded-sm flex-1 p-4 border border-gray-200 flex items-center">c</div>
+            <div className="bg-white rounded-sm flex-1 p-4 border border-gray-200 flex items-center">d</div>
         </div>
     )
+}
+function BoxWrapper({ children }) {
+    return <div className="bg-white rounded-sm flex-1 p-4 border border-gray-200 flex items-center">{children}</div>
 }
