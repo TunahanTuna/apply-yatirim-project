43d707a6e18c51c819551f1b065ecddd9a855a77
Tuna <tuna@applyyatirim.com>
2023-09-01 17:24:42 +0300

    Dashboard created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Route, Router, Routes } from 'react-router-dom'
 import Layout from './components/shared/layout'
-import Dashboard from './components/Dashboard'
-import Oranlar from './components/Oranlar'
+import Dashboard from './pages/Dashboard'
+import Oranlar from './pages/Oranlar'
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Dashboard />} />
                 <Route path="oranlar" element={<Oranlar />} />
+                <Route path="*" element={<Dashboard />} />
             </Route>
         </Routes>
     )
---
+++ src/widgets/ButtonWrapper.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { IoBagHandle } from 'react-icons/io5'
+
+export default function BoxWrapper({ children, title, value, diff, bg }) {
+    console.log(bg)
+    const style = `rounded-full h-12 w-12 flex items-center justify-center bg-${bg}-500`
+    console.log(style)
+    return (
+        <div className="bg-white rounded-sm flex-1 p-4 border border-gray-200 flex items-center hover:outline hover:outline-1 hover:outline-gray-300">
+            <div className={style}>{children}</div>
+            <div className="pl-4 ">
+                <span className="text-sm text-gray-500 font-light">{title && title}</span>
+                <div className="flex items-center">
+                    <strong className="text-lg text-gray-700 font-semibold">{value && value}</strong>
+                    <span className="text-sm text-green-500 pl-2">{diff && diff}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
