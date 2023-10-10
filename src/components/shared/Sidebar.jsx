094425e87f137289bc62dc731fa640ce806343b6
Tuna <tuna@applyyatirim.com>
2023-10-10 15:39:44 +0300

    sidebar mobile design

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -6,6 +6,7 @@ import { texts } from '../../lib/constants/constants'
 import ApplyLogo from '../../assets/ApplyLogo'
 import SideBarTabs from './SideBarTabs'
 import ApplyLogoWhite from '../../assets/ApplyLogoWhite'
+import { FiAlignJustify } from 'react-icons/fi'
 
 export default function Test() {
     const { pathname } = useLocation()
@@ -14,7 +15,15 @@ export default function Test() {
 
     return (
         <div className="h-screen flex flex-col items-end justify-end ">
-            <div className={` lg:w-64 md:w-48  w-0 bg-sky-600 h-screen overflow-y-scroll relative duration-500`}>
+            <button className="z-30 md:invisible visible absolute right-0 m-5 h-14 w-14 rounded-full peer bg-sky-500 hover:bg-sky-600 focus:bg-sky-600 active:bg-sky-700 transition">
+                <span className="text-white items-center flex w-full justify-center">
+                    <FiAlignJustify size={24} />
+                </span>
+            </button>
+
+            <div
+                className={`z-20 md:z-1 fixed md:relative md:left-0 w-72 top-0 -left-96 peer-focus:left-0 peer:transition duration-200 ease-out delay-150 lg:w-64 md:w-48   shadow-2xl bg-sky-600 h-screen overflow-y-scroll `}
+            >
                 <div className="justify-center">
                     <h1
                         className={`text-white font-bold text-2xl text-center duration-200 pt-3 p-5 ${
