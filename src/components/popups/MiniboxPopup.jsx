d8b049bc044faa24195682d210542422e7380fbf
Tuna <tuna@applyyatirim.com>
2023-10-16 15:04:33 +0300

    dashboard update minibox description

--- src/components/popups/MiniboxPopup.jsx
+++ src/components/popups/MiniboxPopup.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import BarCharts from '../BarCharts'
+import classNames from 'classnames'
+import { Hidden } from '@mui/material'
 
 export default function MiniboxPopup({ visible, modal, onClick }) {
     if (!visible) return null
+    const description = modal && modal?.[8]
+    const title = modal && modal?.[1]
+
     return (
         <div
             className="fixed z-30 inset-0 bg-black bg-opacity-20 backdrop-blur-sm xl:flex hidden justify-center items-center"
             onClick={onClick}
         >
-            <div className="bg-orange-50 flex backdrop-blur-sm w-1/2 h-1/2 p-10 rounded-xl">
-                <BarCharts data={modal} />
+            <div
+                className={classNames(
+                    'bg-orange-50 grid 2xl:grid-cols-2 backdrop-blur-sm w-1/2  p-10 gap-4 rounded-xl',
+                    description ? 'grid-cols-2 2xl:grid-cols-2 h-1/2' : 'grid-cols-1 2xl:grid-cols-1 h-3/4'
+                )}
+            >
+                <div className=" hidden 2xl:block">
+                    <BarCharts data={modal} />
+                </div>
+                <div className={classNames('w-full text-center', description ? 'visible' : 'hidden')}>
+                    <h1 className="font-bold text-xl">{title && title}</h1>
+                    <p className="text-justify">{description && description}</p>
+                </div>
             </div>
         </div>
     )
--- src/components/sections/DashboardSecondSection.jsx
+++ src/components/sections/DashboardSecondSection.jsx
@@ -2,14 +2,30 @@ import React from 'react'
 import MiniBox from '../dashboards/MiniBox'
 import { BarCharts, RatioChart } from '../index'
 import DonutChart from '../dashboards/DonutChart'
+import MiniboxPopup from '../popups/MiniboxPopup'
+import { useState } from 'react'
 
 export default function DashboardSecondSection({ data }) {
     const { totalRevenue, TLRevenue, totalReveneu, cariOran, NetIsletmeSermayesi, table_title, toplamVarliklar } = data
+    const [modal, setModal] = useState([])
+    const [open, setOpen] = useState(false)
     return (
         <div className="w-full 2xl:flex grid lg:grid-cols-2 md:grid-cols-1  gap-2">
             <div className="flex flex-1 flex-col gap-1">
-                <MiniBox data={totalRevenue && totalRevenue} />
-                <MiniBox data={TLRevenue && TLRevenue} />
+                <MiniBox
+                    data={totalRevenue && totalRevenue}
+                    onClick={() => {
+                        setModal(totalRevenue)
+                        setOpen(true)
+                    }}
+                />
+                <MiniBox
+                    data={TLRevenue && TLRevenue}
+                    onClick={() => {
+                        setModal(TLRevenue)
+                        setOpen(true)
+                    }}
+                />
             </div>
             <div className="flex flex-1 ">
                 <BarCharts data={totalReveneu && totalReveneu} table_title={table_title && table_title} />
@@ -20,6 +36,7 @@ export default function DashboardSecondSection({ data }) {
             <div className="flex flex-1">
                 <DonutChart data={toplamVarliklar && toplamVarliklar} table_title={table_title && table_title} />
             </div>
+            <MiniboxPopup visible={open} modal={modal} onClick={() => setOpen(false)} />
         </div>
     )
 }
